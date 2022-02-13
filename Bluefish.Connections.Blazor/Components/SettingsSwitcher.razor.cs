7be00675447cb1a4053042b44e34ce88c5b65f85
Mark Goldring <markgoldring@hotmail.com>
2022-02-13 17:26:47 +0000

    Connections.Blazor - Added SettingsSwitch component

---
+++ Bluefish.Connections.Blazor/Components/SettingsSwitcher.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace Bluefish.Connections.Blazor.Components;
+
+public partial class SettingsSwitcher
+{
+    [Parameter]
+    public bool CanEdit { get; set; } = true;
+
+    [Parameter]
+    public string ConnectionType { get; set; } = typeof(LocalFileSystemConnection).FullName ?? String.Empty;
+
+    [Parameter]
+    public string CssClass { get; set; } = "mb-2";
+
+    [Parameter]
+    public string Settings { get; set; } = String.Empty;
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+}
